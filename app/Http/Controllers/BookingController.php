26c00b27cd8790d72cbfd6aab9960dd954eb7ae8
islamshu12 <islamshu12@gmail.com>
2023-05-31 17:21:05 +0300

    edit

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -133,9 +133,10 @@ class BookingController extends Controller
         $booking=Booking::where('code',$booking)->first();
         return view('frontend.pdf')->with('booking',$booking);
     }
-    public function edit(Booking $booking)
+    public function edit($id)
     {
-        //
+        $booking=Booking::find($id);
+        return view('dashboard.booking.edit')->with('booking',$booking);
     }
 
     /**
@@ -145,9 +146,83 @@ class BookingController extends Controller
      * @param  \App\Models\Booking  $booking
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Booking $booking)
+    public function update(Request $request, $id)
     {
-        //
+        $booking = Booking::find($id);
+        $booking->type_offer = $request->type_offer;
+        $booking->destination = $request->destination;
+        $booking->name = $request->name;
+        $booking->phone = $request->phone;
+        $booking->date_of_arrival = $request->date_of_arrival;
+        $booking->night_number = $request->night_number;
+        $booking->adult = $request->adult;
+        $booking->child = $request->child;
+        $booking->date_of_departure = $request->date_of_departure;
+        $booking->price = $request->price;
+        if($request->check_Bok){
+            $booking->show_arraive = 1;
+        }else{
+            $booking->show_arraive = 0;
+        }
+        $booking->code = date('Ymd-His').rand(10,99);
+        $booking->save();
+        if(isset($request->hotel)){
+            Hotel::where('booking_id',$id)->delete();
+            foreach($request->hotel as $hotel){
+                if($hotel == null){
+                    continue;
+                }
+                $hotell = new Hotel();
+                $hotell->city = $hotel['city'];
+                $hotell->name = $hotel['name'];
+                $hotell->type = $hotel['type'];
+                $hotell->night = $hotel['night'];
+                $hotell->eat = $hotel['eat'];
+                $hotell->booking_id = $booking->id;
+                $hotell->save();
+            }
+        }
+        if(isset($request->note)){
+            BookingNote::where('booking_id',$id)->delete();
+
+            foreach($request->note as $note){
+                if($note['title'] == null){
+                    continue;
+                }
+                $notee = new BookingNote();
+                $notee->title = $note['title'];
+                $notee->booking_id = $booking->id;
+                $notee->save();
+            }
+        }
+        if(isset($request->detiles)){
+            BookingDetiels::where('booking_id',$id)->delete();
+
+            foreach($request->detiles as $detiles){
+                if($detiles['title'] == null){
+                    continue;
+                }
+                $dee = new BookingDetiels();
+                $dee->title = $detiles['title'];
+                $dee->booking_id = $booking->id;
+                $dee->save();
+            }
+        }
+        if(isset($request->arraive)){
+            BookingArraive::where('booking_id',$id)->delete();
+
+            foreach($request->arraive as $arraive){
+                if($arraive['title'] == null){
+                    continue;
+                }
+                $arraivee = new BookingArraive();
+                $arraivee->title = $arraive['title'];
+                $arraivee->check = 1;
+                $arraivee->booking_id = $booking->id;
+                $arraivee->save();
+            }
+        }
+        return redirect()->back()->with(['success'=>'تم الحفظ بنجاح']);
     }
 
     /**
